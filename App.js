bf85e9e639a4506d829cd67e99dd9669d6de21b5
1k32k2 <hung01237160439@gmail.com>
2022-10-27 11:55:35 +0700

    Update Readme.md and fix some bug

--- App.js
+++ App.js
@@ -1,31 +1,114 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+// App.js
+import { React } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default class App extends Component 
-{
-  //Hàm khởi tạo
-  constructor()
-  {
-    super()
-    this.state = {resultText: "", calculationText: ""}
-    this.operations = ['D','+','-','*','/']
-  }
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-  delcalc(temp)
+import { SafeAreaView,StyleSheet, Text, View, Button, TouchableOpacity, Dimensions } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width
+const windowheight = Dimensions.get('window').height
+
+  let  resultText= " "
+  let  calculationText= " "
+  let  historyText= ""
+
+  let operations = ['D','+','-','*','/'] //Các nút ở cột màu đen
+  let nums = [['C','+/-','%'],[1,2,3],[4,5,6],[7,8,9],['.',0,'=']] //Các nút ở phần màu vàng
+
+
+  //Khu vực thay đổi UI
+const styles = StyleSheet.create({
+  container: {
+    width: windowWidth,
+    height: windowheight,
+    flex:1
+  },
+  //Kích cỡ chữ nhập vào (khung màu đỏ)
+  resultText:{
+    fontSize:30,
+    color: 'white'
+  },
+  //Vị trí các nút
+  btn:{
+    flex:1,
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  //các nút C +/- % 1 2 3 4 5 ...
+  btntext: {
+    fontSize:30,
+    color: 'black'
+  },
+  //các nút D + - * / F
+  white: {
+    fontSize: 30,
+    color: 'white'
+  },
+  //Kích cỡ chữ xuất ra(khung màu xanh lá)
+  calculationText:{
+    fontSize:24,
+    color: 'white'
+  },
+
+  row:{
+      flexDirection: 'row',
+      flex: 1,
+      justifyContent: 'space-around',
+      alignContent: 'center'
+  },
+  //khung input(màu đỏ)
+  result: {
+    marginTop: 0,
+    flex: 3,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'flex-end'
+  },
+  //khung output(màu xanh lá)
+  calculation: {
+    marginTop: 0,
+    flex: 2,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'flex-end'
+  },
+  buttons: {
+    flexGrow:7,
+    flexDirection: 'row'
+  },
+  //Các nút 1 2 3 4 5 ...
+  numbers: {
+    flex:3,
+    backgroundColor: 'yellow'
+  },
+  //các nút D + - * / F
+  operations: {
+    flex:1,
+    justifyContent: 'space-around',
+    alignItems: 'stretch',
+    backgroundColor: 'black'
+  },
+
+})
+
+  // Hàm xóa hết ('C","+/-","%")
+  function delcalc(temp)
   {
     switch(temp)
     {
       case 'C':
-        this.setState({calculationText: "",resultText: ""})
+        calculationText = " "
+        resultText= " "
         break
-      case 'D':
-        console.log(this.state.resultText)
-        let text = this.state.resultText.split('')
-        text.pop()
-        this.setState({ resultText: text.join('')})
+      case '+/-':
+        resultText= resultText*-1
         break
       case'%':
-        this.setState({calculationText: eval(this.state.resultText/100)})
+        resultText=resultText+"/100"
         break
     }
 
@@ -33,16 +116,16 @@ export default class App extends Component
   }
 
   // Hàm tính toán
-  calculateResult()
+  function calculateResult()
   {
-    const text = this.state.resultText
+    const text = resultText
     //console.log(text,eval(text))
-    this.setState({calculationText: eval(text)})
+    calculationText = eval(text)
   }
 
-  validate()
+  function validate()
   {
-    const text = this.stage.resultText
+    const text = resultText
     switch(text.slice(-1))
     {
       case '+':
@@ -54,26 +137,29 @@ export default class App extends Component
   }
   
   //các nút 123456789.0=
-  buttonPressed(text)
+  function buttonPressed(text)
   {
     //console.log(text)
     if(text == '=')
     {
-      return this.calculateResult() //Nếu nhấn = thì sẽ tính
+      return calculateResult() //Nếu nhấn = thì sẽ tính
     }
-    this.setState({resultText: this.state.resultText+text}) // Xuất ký tự đã nhập trên khung màu đỏ
+    resultText = resultText+text // Xuất ký tự đã nhập trên khung màu đỏ
   }
 
-  //các nút D+-*/
-  operate(operation)
+  //Xử lý các nút D+-*/F
+  function operate(operation)
   {
     switch(operation)
     {
       case 'D':
-        console.log(this.state.resultText)
-        let text = this.state.resultText.split('')
-        text.pop()
-        this.setState({ resultText: text.join('')})
+        if(resultText!=" ")
+        {
+          console.log(resultText)
+          let text = resultText.split('')
+          text.pop()
+          resultText = text.join('')
+        }
         break
       case '+':
       case '-':
@@ -82,28 +168,27 @@ export default class App extends Component
         //const lastChar = this.state.text.split('').pop()
         //if(this.operations.indexOf(lastChar) >0) return
         //if(this.state.text == ''|| this.state.text.split('').pop()) return
-        this.setState({resultText: this.state.resultText + operation})
+        resultText= resultText + operation
+        break
     }
   }
 
-  render()
-  {
+
     let rows = []
-    let nums = [['C','D','%'],[1,2,3],[4,5,6],[7,8,9],['.',0,'=']]
     let row = []
-    for(let i =0; i<3;i++)
+    for(let i =0; i<3;i++)//Vòng lặp tạo các nút C,+/-,%
     {
-      row.push(<TouchableOpacity onPress={() => this.delcalc(nums[0][i])} style={styles.btn}>
+      row.push(<TouchableOpacity onPress={() => delcalc(nums[0][i])} style={styles.btn}>
                   <Text style={styles.btntext}>{nums[0][i]}</Text>
                 </TouchableOpacity>)
     }
     rows.push(<View style={styles.row}>{row}</View>)
-    for(let i=1; i<5; i++)
+    for(let i=1; i<5; i++)//Vòng lặp tạo các nút 1,2,3,4,5,6,7,8,9,.,0,=
     {
       let row = []
       for(let j=0; j<3; j++)
       {
-        row.push(<TouchableOpacity onPress={() => this.buttonPressed(nums[i][j])} style={styles.btn}>
+        row.push(<TouchableOpacity onPress={() => buttonPressed(nums[i][j])} style={styles.btn}>
                   <Text style={styles.btntext}>{nums[i][j]}</Text>
                 </TouchableOpacity>)
       }
@@ -112,88 +197,73 @@ export default class App extends Component
 
     
     let ops = []
-    for(let i = 0; i<5; i++)
+    for(let i = 0; i<5; i++)//Vòng lặp tạo các nút D,+,-,*,/,F
     {
-      ops.push(<TouchableOpacity style={styles.btn} onPress={() => this.operate(this.operations[i])} >
-                  <Text style={[styles.btntext, styles.white]}>{this.operations[i]}</Text>
+      ops.push(<TouchableOpacity style={styles.btn} onPress={() => operate(operations[i])} >
+                  <Text style={[styles.btntext, styles.white]}>{operations[i]}</Text>
                 </TouchableOpacity>)
     }
+    //ops.push(<TouchableOpacity style={styles.btn} onPress={() => navigation.navigate(SettingsScreen)} >
+    //        <Text style={[styles.btntext, styles.white]}>{'F'}</Text>
+    //        </TouchableOpacity>)
+
+
+
+function calc()
+    {
+      return (
+        <View style = {styles.container}>
+          <View style={styles.result}>  
+            <Text style={styles.resultText}>{resultText}</Text>
+          </View>
+          <View style={styles.calculation}>
+            <Text style={styles.calculationText}>{calculationText}</Text>
+          </View>
+          <View style={styles.buttons}>
+            <View style={styles.numbers}>
+              {rows}
+            </View>
+            <View style={styles.operations}>
+              {ops}
+            </View>
+          </View>
+        </View>
+      );
+  }
+
+  function SettingsScreen() {
     return (
-      <View style = {styles.container}>
-        <View style={styles.result}>  
-          <Text style={styles.resultText}>{this.state.resultText}</Text>
-        </View>
-        <View style={styles.calculation}>
-          <Text style={styles.calculationText}>{this.state.calculationText}</Text>
-        </View>
-        <View style={styles.buttons}>
-          <View style={styles.numbers}>
-            {rows}
-          </View>
-          <View style={styles.operations}>
-            {ops}
-          </View>
-        </View>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
       </View>
     );
   }
+
+
+
+const Tab = createBottomTabNavigator();
+
+function calculator()
+{
+  <NavigationContainer>
+      <calc />
+    </NavigationContainer>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex:1
-  },
-  resultText:{
-    fontSize:30,
-    color: 'white'
-  },
-  btn:{
-    flex:1,
-    alignItems: 'center',
-    alignSelf: 'stretch',
-    justifyContent: 'center'
-  },
-  btntext: {
-    fontSize:30 
-  },
-  white: {
-    color: 'white'
-  },
-  calculationText:{
-    fontSize:24,
-    color: 'white'
-  },
-  row:{
-      flexDirection: 'row',
-      flex: 1,
-      justifyContent: 'space-around',
-      alignContent: 'center'
-  },
-  result: {
-    flex:2,
-    backgroundColor: 'red',
-    justifyContent: 'center',
-    alignItems: 'flex-end'
-  },
-  calculation: {
-    flex:1,
-    backgroundColor: 'green',
-    justifyContent: 'center',
-    alignItems: 'flex-end'
-  },
-  buttons: {
-    flexGrow:7,
-    flexDirection: 'row'
-  },
-  numbers: {
-    flex:3,
-    backgroundColor: 'yellow'
-  },
-  operations: {
-    flex:1,
-    justifyContent: 'space-around',
-    alignItems: 'stretch',
-    backgroundColor: 'black'
-  },
+function MyTabs() {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Tab.Navigator>
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </SafeAreaView>   
+  );
+}
 
-})
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # simple-calculator
-a simple calculator 
+
+A simple calculator built with React Native
+
+## Installation
+
+Step 1: Clone this repo, open CMD and change directory to the folder you have cloned, then input
+
+```bash
+npm install
+```
+to install React native to this folder
+
+ <br />
+
+Step 2: Run 
+```bash
+npm start
+```
+to run the app
+<br />
+##### Note: if you want to run that app on web, you must run ```npx expo install react-native-web@~0.18.7 react-dom@18.0.0 @expo/webpack-config@^0.17.0``` at step 1
+
+<br />
+
+Step 3: 
++ If you run on web, just press `W` and a browser will open with `localhost` site
++ If you want to run on an Android/iOS phone:
+  1. You must connecting the phone to the same network as your computer and sure that Expo Go is installed on it
+  2. Open the app and scan the QR code on the CMD
+  3. The app should run now
